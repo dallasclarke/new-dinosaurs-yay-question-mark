cfc1181d1b2ca8e4204499c86f71ed0899956ff8
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-07-23 00:28:58 -0700

    hw

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
-const makeDino = function() {
+
+
+
+const makeDino = function(species, period, diet, existence) {
+  
+  if (existence === undefined) {
+    existence = false
+  }
+    return {
+      carnivore: diet,
+      species: species,
+      period: period,
+      extinct: existence,
+    }
+  }
+
+
+
+const makeSingular = function(dino) {
+  // Using Object.assign method to clone object so no mutation occurs.
+  const dinoCopy = Object.assign({}, dino);
+
+  const dinoSpecies = dinoCopy.species
+
+  if (dinoSpecies.slice(dinoSpecies.length -2) === 'us') {
+    dinoCopy.species = dinoSpecies.slice(0, dinoSpecies.length -2)
+  }
+
+  return dinoCopy  
 }
 
-const makeSingular = function() {
+const truncateSpecies = function(dino) {
+  const dinoCopy = Object.assign({}, dino);
+  const dinoSpecies = dinoCopy.species
+
+  if (dinoSpecies.length > 10 ) {
+    dinoCopy.species = dinoSpecies.slice(0, 7) + '...'
+  } 
+  return dinoCopy
 }
 
-const truncateSpecies = function() {
-}
+const makeExtinct = function(dino) {
+  const dinoCopy = Object.assign({}, dino);
+  // const dinoExistence = dinoCopy.extinct
 
-const makeExtinct = function() {
+ 
+
+  return dinoCopy
 
 }
 
